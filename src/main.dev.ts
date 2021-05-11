e83d4d077acb0428b05bd71088a7ffb3d8f86cf6
Joshua Stephen <jostephen22@yahoo.com>
2021-05-11 16:42:19 +0700

    fix: enable devtools extensions

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -40,13 +40,15 @@ if (
 
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   const extensions = ['REACT_DEVELOPER_TOOLS'];
 
   return installer
     .default(
       extensions.map((name) => installer[name]),
-      forceDownload
+      {
+        loadExtensionOptions: { allowFileAccess: true },
+        forceDownload: false,
+      }
     )
     .catch(console.log);
 };
